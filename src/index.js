26b68fdb6de19c6e4366ac47ff5e4cbc6ae8f54d
olgakold <64192555+olgakold@users.noreply.github.com>
2020-10-18 23:42:17 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    for (let n=0; n<100; n++){
+    for (let i=0; i<bracketsConfig.length; i++){
+       
+       while (str.indexOf(bracketsConfig[i].join('')) !== -1){
+         str = str.replace(bracketsConfig[i].join(''),'')
+       }     
+      }
+    }
+   
+    return str.length > 0 ? false : true
+  }
